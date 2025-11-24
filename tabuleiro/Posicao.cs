b2d2cee56b785e8c2add21d1559a802e30ecfd93
Jurandi Junior <jurandijr20@gmail.com>
2025-11-24 16:04:21 -0300

    Classe Posicao criada

---
+++ tabuleiro/Posicao.cs
@@ -0,0 +1,22 @@
+﻿namespace tabuleiro
+{
+    internal class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+
+        public Posicao(int Linha, int Coluna)
+        {
+            this.linha = Linha;
+            this.coluna = Coluna;
+        }
+
+        public override string ToString()
+        {
+            return linha 
+                + ", " 
+                + coluna;
+        }
+    }
+}
